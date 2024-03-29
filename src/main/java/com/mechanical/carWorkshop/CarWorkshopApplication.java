1ed06b666a9b7883569f49ba3e535136ab39267e
Leandro Dolensi <leandro.dolensi@aluno.ifsp.edu.br>
2024-03-29 09:52:55 -0300

    init project car mechanical workshop api

---
+++ src/main/java/com/mechanical/carWorkshop/CarWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.mechanical.carWorkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarWorkshopApplication.class, args);
+	}
+
+}
